6e2d27619cb47e229fb7d223533561e0ff523cc3
jorgemuriel <jorge.muriel@taksee.com>
2016-12-18 17:07:18 +0100

    UPDATE Test 1

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def makeWebhookResult(data):
 
     # print(json.dumps(item, indent=4))
 
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+    speech = "Hoy la temperatura es de " + location.get('city') + ": " + condition.get('text') + \
              ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
 
     print("Response:")
